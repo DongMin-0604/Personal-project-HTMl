857de2b9d9e89f2e49b7f1771ee9c512ee095062
DongMin-0604 <dongmin3147@gmail.com>
2021-08-05 17:23:21 +0900

    JS파일 생성 a태그 클릭시 이벤트 함수 테스트,카테고리 탭 삭제

---
+++ 자율동아리/main.js
@@ -0,0 +1,9 @@
+var Display = true;
+function doDisplay(){
+    var con = document.getElementById("Assassin_info_T")
+    if(con.style.display == "none"){
+        con.style.display = 'block';
+    }else{
+        con.style.display = 'none';
+    }
+}
